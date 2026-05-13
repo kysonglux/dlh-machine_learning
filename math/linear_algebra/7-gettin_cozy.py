dbee788e460dcbafca1486ab9e7212d7f2a3596a
kysong <kyoungran.song@ai.dlh.lu>
2026-05-13 15:52:05 +0000

    7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,9 +4,14 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """"shallow copy vs. deep copy """
+      # Check if either matrix is empty
+       # Check all rows in mat1 have same length
+         # Check all rows in mat2 have same length
     if axis == 0:
+           # Must have same number of columns
         return [row[:] for row in mat1] + [row[:] for row in mat2]
     else:
+         # Must have same number of rows
         if len(mat1) != len(mat2):
             return None
         new_matrix = []
