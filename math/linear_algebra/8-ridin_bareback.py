3f053775b047319000a7eed50f9c4da03ce7fcec
kysong <kyoungran.song@ai.dlh.lu>
2026-05-14 09:08:27 +0000

    8-ridin_bareback.py

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" matrix multiplication """
+
+
+def mat_mul(mat1, mat2):
+    """ matrix multiplication """
+    rows_mat1 = len(mat1)
+    cols_mat1 = len(mat1[0])
+    rows_mat2 = len(mat2)
+    cols_mat2 = len(mat2[0])
+
+    if cols_mat1 != rows_mat2:
+        raise ValueError("Incompatible matrix dimenstions")
+
+    result = [[0 for _ in range(cols_mat2)] for _ in range(rows_mat1)]
+    for i in range(rows_mat1):
+        for j in range(cols_mat2):
+            for k in range(cols_mat1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
