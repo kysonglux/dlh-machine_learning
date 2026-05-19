5c28aaef1fef90446a5cdebdf29b18995a424c00
kysong <kyoungran.song@ai.dlh.lu>
2026-05-19 14:17:34 +0200

    101-the_whole_barn.py

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" adds two matrices element-wise """
+
+
+def add_matrices(mat1, mat2):
+    """Adds two matrices element-wise """
+    if isinstance(mat1, int) and isinstance(mat2, int):
+        return mat1 + mat2
+    if isinstance(mat1, list) and isinstance(mat2, list):
+        if len(mat1) != len(mat2):
+            return None
+        return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
+
+    return None
