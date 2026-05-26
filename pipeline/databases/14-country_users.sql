fe9c4890c0545aada4329d50642cbd4e9ac1aa20
kysong <kyoungran.song@ai.dlh.lu>
2026-05-26 12:36:25 +0200

    14-country_users.sql

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- create a table and insert enumeration data type
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
