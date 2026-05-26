aa2b053ad626dca9ef5d11def2cc6fbe8ae28a71
kysong <kyoungran.song@ai.dlh.lu>
2026-05-26 11:32:34 +0200

    11-rating_shows.sql

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,4 +1,4 @@
---lists all shows by rating
+-- lists all shows by rating
 SELECT tv_shows.title , sum(tv_show_ratings.rate) AS rating 
 FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
