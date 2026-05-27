1be1f127d3812c848dfb680d1ec0a9e6d51ef417
kysong <kyoungran.song@ai.dlh.lu>
2026-05-27 14:55:12 +0200

    20-average_score.sql

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END$$
+
+DELIMITER ;
