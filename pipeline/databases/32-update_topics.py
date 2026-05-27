8cf7cd26311cabdaa8549f443a1ffaef529ce8cb
kysong <kyoungran.song@ai.dlh.lu>
2026-05-27 18:26:29 +0200

    32-update_topics.py

---
+++ pipeline/databases/32-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""update"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topic": topics}}
+    )
