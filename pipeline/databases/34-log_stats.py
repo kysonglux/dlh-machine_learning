c998a83145b29807114cb6b2155443524e2e6482
kysong <kyoungran.song@ai.dlh.lu>
2026-05-27 21:41:49 +0200

    34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -8,16 +8,18 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    # Total logs
+    total = collection.count_documents({})
+    print(f"{total} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for m in methods:
         count = collection.count_documents({"method": m})
-        print(f"\tmethod {m}: {count}")
+        print(f"    method {m}: {count}")
 
-    status_check = collection.count_documents(
+    # GET /status
+    status = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_check} status check")
+    print(f"{status} status check")
