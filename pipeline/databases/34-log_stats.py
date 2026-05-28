5828d94e87b1bced5b0b886bbbf1ae6c22e6ca87
kysong <kyoungran.song@ai.dlh.lu>
2026-05-28 17:59:39 +0200

    34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
 """Provides stats about Nginx logs stored in MongoDB"""
-
-
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    # Total logs
     total = collection.count_documents({})
     print(f"{total} logs")
 
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for m in methods:
+    for m in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": m})
         print(f"\tmethod {m}: {count}")
 
-    # GET /status
-    status = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    status = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status} status check")
