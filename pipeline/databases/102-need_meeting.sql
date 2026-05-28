bf94b7cc42dc2c1a0658b54b068e8f589b6a5871
kysong <kyoungran.song@ai.dlh.lu>
2026-05-28 18:07:38 +0200

    102-need_meeting.sql

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -3,4 +3,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH)
+ AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
