653ab0f7f5d4e3cfb2c1137862974f0ee8a7ba8d
kysong <kyoung-ran.song@ai.dlh.lu>
2026-06-04 16:34:02 +0200

    17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,4 +4,13 @@
 
 def poly_integral(poly, C=0):
     """ calculates the integral of a polynomial """
-    
+    if not isinstance(poly, list) or poly == []:
+       return None 
+    if not isinstance(C, int):
+        return None
+    integral = []
+    for i in range(1, len(poly)):
+        integral.append( 1 / (poly[i] + 1))
+    if len(poly) > 1 and poly == 0:
+        integral.pop()
+    return integral    
