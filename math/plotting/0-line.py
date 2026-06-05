fbc7a8dcc733735f49959839c764418ff77cb27f
kysong <kyoungran.song@ai.dlh.lu>
2026-06-05 11:28:15 +0200

    0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,7 +8,6 @@ def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = list(range(11))
-    x = np.linspace(0, 10, 2)
-    plt.plot(x, y)
-    plt.show()
+    x = np.arange(0, 10, 2)
+    plt.plot(y)
+    plt.savefig("line_plot.png")
