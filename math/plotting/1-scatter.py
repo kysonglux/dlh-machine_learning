47dd83cdb057a650a49257ca849ad48020a1c512
kysong <kyoungran.song@ai.dlh.lu>
2026-06-05 12:29:39 +0200

    1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,8 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter():
 
+def scatter():
+    """draw scatter chart"""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -11,4 +12,8 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    # your code here
+    plt.scatter(x, y, color="magenta")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
+    plt.savefig("scatter.png")
