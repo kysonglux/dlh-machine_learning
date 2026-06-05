f6b7f28853fed6637dda75c3e3e2a2d47a9b675d
kysong <kyoungran.song@ai.dlh.lu>
2026-06-05 14:51:34 +0200

    0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -10,7 +10,7 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
-    plt.plot(y, color="red", linewidth=2)
+    plt.plot(y, color="red")
 
     plt.xlim(0, 10)
     plt.savefig("line_plot.png")
