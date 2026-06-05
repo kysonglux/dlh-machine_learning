3fba0d60ff4db5136b748f5d92b980ff5d205ad2
kysong <kyoungran.song@ai.dlh.lu>
2026-06-05 16:15:44 +0200

    2-change_scale.py

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -15,7 +15,7 @@ def change_scale():
 
     plt.plot(x, y, color="blue")
     plt.yscale("log")
-    plt.xlim(0, 28650)
+    plt.xlim(0, 28651)
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of C-14")
