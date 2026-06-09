ced2243efea00bc6eb6dc861ae87ed4ca815f721
kysong <kyoungran.song@ai.dlh.lu>
2026-06-09 06:47:12 +0000

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+""" Create Poisson distribution"""
+
+
+def __init__(self, data=None, lambtha=1.):
+    """ Create Poisson distribution """
+    if not isinstance(data, list):
+        raise TypeError ("data must be a list")
+    if count(data) < 2:
+        raise ValueError("data must contain multiple values")
+    if data == None:
+        data = lambtha
+    if lambtha < 0 or lambtha == 0:
+        raise ValueError(("lambtha must be a positive value"))
+    else:
+        return (e^(-lambtha)*(lambtha^data)/(data!))
+
