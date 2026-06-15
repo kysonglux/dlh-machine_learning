0d4e0e9807779e27c4b498b694a70dedafca0414
kysong <kyoungran.song@ai.dlh.lu>
2026-06-15 22:20:29 +0200

    0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Calculates the likelihood"""
+import numpy as np
 
 
 def likelihood(x, n, P):
@@ -7,15 +8,19 @@ def likelihood(x, n, P):
     if n <= 0:
         raise ValueError("n must be a positive integer")
     if x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.dnim != 1:
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if P >= 0 and P <= 1:
+    if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
-    
-    coefficient = n!/(x!(n-x)!)
-    successes = P **x
-    failures = (1 - P) **(n - x)
+
+    k = min(x, n-x)
+    coefficient = 1
+    for i in range(1, k + 1):
+        coefficient = coefficient * (n - k + i) // i
+    successes = P ** x
+    failures = (1 - P) ** (n - x)
     return coefficient * successes * failures
