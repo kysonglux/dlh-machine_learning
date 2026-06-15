6fcdf3c361217f33be6226ba36d77690b919c56c
kysong <kyoungran.song@ai.dlh.lu>
2026-06-15 22:23:20 +0200

    0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -5,9 +5,9 @@ import numpy as np
 
 def likelihood(x, n, P):
     """Calculates the likelihood"""
-    if n <= 0:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if x < 0:
+    if not isinstance(x, int) or x < 0:
         raise ValueError("x must be an integer that is "
                          "greater than or equal to 0")
     if x > n:
