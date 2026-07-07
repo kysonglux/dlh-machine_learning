44003178e32b1d8f4736be3a16fdde12a421ab1e
kysong <kyoungran.song@ai.dlh.lu>
2026-07-07 12:53:11 +0200

    0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def from_numpy(array):
-    """creates dataframe"""
+    """creates dataframe with columns index"""
     cols = array.shape[1]
     columns = [chr(ord('A') + i) for i in range(cols)]
     df = pd.DataFrame(array, columns=columns)
