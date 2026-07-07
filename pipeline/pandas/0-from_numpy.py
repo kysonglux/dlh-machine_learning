39ea6200cd8d6387dec409ac7db7a1737867be39
kysong <kyoungran.song@ai.dlh.lu>
2026-07-07 12:48:51 +0200

    0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import numpy as np
 
 
 def from_numpy(array):
     """creates dataframe"""
-    data = np.array(array)
     cols = array.shape[1]
     columns = [chr(ord('A') + i) for i in range(cols)]
-    df = pd.DataFrame(data, columns=columns)
+    df = pd.DataFrame(array, columns=columns)
     return df
